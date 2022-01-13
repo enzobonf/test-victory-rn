4d185178a23435fe705203be40acef854f3e1cb4
Enzo Bonfante <enzobonfx@gmail.com>
2022-01-13 16:47:55 -0300

    update

--- App.tsx
+++ App.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 import React from 'react';
 import {Dimensions, ScrollView, StyleSheet, View} from 'react-native';
-import {VictoryBar, VictoryChart, VictoryTheme} from 'victory-native';
+import {
+  VictoryArea,
+  VictoryBar,
+  VictoryChart,
+  VictoryLine,
+  VictoryTheme,
+  VictoryZoomContainer,
+} from 'victory-native';
 
 const getRandom = (min: any, max: any) => {
   return Math.floor(Math.random() * (max + 1 - min) + min);
@@ -9,7 +16,7 @@ const getRandom = (min: any, max: any) => {
 
 const gerarDadosAleatorios = () => {
   const data: any[] = [];
-  for (let i = 1; i <= 15; i++) {
+  for (let i = 1; i <= 39; i++) {
     let obj = {mortalidade: getRandom(i, 130), idade: i};
     data.push(obj);
   }
@@ -63,10 +70,11 @@ export default class App extends React.Component {
           <VictoryChart
             width={dimensions.screen.width}
             theme={VictoryTheme.material}
-            scale="linear">
+            containerComponent={<VictoryZoomContainer />}>
             <VictoryBar
-              samples={100}
-              barWidth={20}
+              sortKey="y"
+              sortOrder="ascending"
+              barRatio={0.9}
               data={data}
               x="idade"
               y="mortalidade"
@@ -76,6 +84,12 @@ export default class App extends React.Component {
                   fill: '#b9d3f8',
                 },
               }}
+            />
+            <VictoryLine
+              data={data}
+              x="idade"
+              y="mortalidade"
+              labels={({datum}) => datum.mortalidade}
             />
           </VictoryChart>
         </View>
